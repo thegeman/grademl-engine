4450b8c396a1b242e5999bf4e1a792558d6c5a27
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-04-26 23:27:03 +0200

    Add submodule for parsing Resource Monitor metric files

---
+++ grademl-input/grademl-input-resource-monitor/build.gradle.kts
@@ -0,0 +1,9 @@
+description = "Parsing for Resource Monitor metric files"
+
+plugins {
+    kotlin("jvm") version "1.5.0-RC"
+}
+
+dependencies {
+    implementation(project(":grademl-core"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "grademl-engine"
 
 include(":grademl-core")
 include(":grademl-input:grademl-input-airflow")
+include(":grademl-input:grademl-input-resource-monitor")
