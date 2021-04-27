e115b371faeca522e1b37325e9b135bc82221a13
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-04-28 01:27:12 +0200

    Refactor PlotOverviewCommand

--- grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/PlotOverviewCommand.kt
+++ grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/PlotOverviewCommand.kt
@@ -7,9 +7,6 @@ import science.atlarge.grademl.cli.data.PhaseListWriter
 import science.atlarge.grademl.cli.util.ParsedCommand
 import science.atlarge.grademl.cli.util.instantiateRScript
 import science.atlarge.grademl.cli.util.runRScript
-import science.atlarge.grademl.core.execution.ExecutionPhase
-import science.atlarge.grademl.core.resources.Metric
-import java.nio.file.Path
 
 object PlotOverviewCommand : Command(
     name = "plot-overview",
@@ -27,69 +24,42 @@ object PlotOverviewCommand : Command(
         val rScriptDirectory = outputDirectory.resolve(".R").also { it.toFile().mkdirs() }
 
         // Select and output all phases in the execution model
-        writePhaseList(
+        val phaseListFile = dataOutputDirectory.resolve(PhaseListWriter.FILENAME).toFile()
+        println("Writing list of execution phases to \"${phaseListFile.absolutePath}\".")
+        PhaseListWriter.output(
+            phaseListFile,
             cliState.executionModel.rootPhase,
             cliState.executionModel.phases,
-            cliState,
-            dataOutputDirectory
+            cliState
         )
 
         // Select and output all metrics in the resource model
-        writeMetricList(
+        val metricListFile = dataOutputDirectory.resolve(MetricListWriter.FILENAME).toFile()
+        println("Writing list of metrics to \"${metricListFile.absolutePath}\".")
+        MetricListWriter.output(
+            metricListFile,
             cliState.resourceModel.resources.flatMap { it.metrics },
-            cliState,
-            dataOutputDirectory
+            cliState
         )
 
         // Write all metric data as time series to a file
-        writeMetricData(
+        val metricDataFile = dataOutputDirectory.resolve(MetricDataWriter.FILENAME).toFile()
+        println("Writing metric data to \"${metricDataFile.absolutePath}\".")
+        MetricDataWriter.output(
+            metricDataFile,
             cliState.resourceModel.resources.flatMap { it.metrics },
-            cliState,
-            dataOutputDirectory
+            cliState
         )
 
         // Instantiate the plot script template
         val rScriptFile = rScriptDirectory.resolve(SCRIPT_FILENAME).toFile()
         val plotOutputFile = dataOutputDirectory.resolve(PLOT_FILENAME).toFile()
         println("Instantiating R script to \"${rScriptFile.absolutePath}\".")
-        instantiateRScript(rScriptFile, mapOf(
-            "plot_filename" to plotOutputFile.name
-        ))
+        instantiateRScript(rScriptFile, mapOf("plot_filename" to plotOutputFile.name))
 
         // Plot the overview using R
         println("Generating plot to \"${plotOutputFile.absolutePath}\".")
         runRScript(rScriptFile)
-    }
-
-    private fun writePhaseList(
-        rootPhase: ExecutionPhase,
-        selectedPhases: Iterable<ExecutionPhase>,
-        cliState: CliState,
-        dataOutputPath: Path
-    ) {
-        val outputFile = dataOutputPath.resolve(PhaseListWriter.FILENAME).toFile()
-        println("Writing list of execution phases to \"${outputFile.absolutePath}\".")
-        PhaseListWriter.output(outputFile, rootPhase, selectedPhases, cliState)
-    }
-
-    private fun writeMetricList(
-        selectedMetrics: Iterable<Metric>,
-        cliState: CliState,
-        dataOutputPath: Path
-    ) {
-        val outputFile = dataOutputPath.resolve(MetricListWriter.FILENAME).toFile()
-        println("Writing list of metrics to \"${outputFile.absolutePath}\".")
-        MetricListWriter.output(outputFile, selectedMetrics, cliState)
-    }
-
-    private fun writeMetricData(
-        selectedMetrics: Iterable<Metric>,
-        cliState: CliState,
-        dataOutputPath: Path
-    ) {
-        val outputFile = dataOutputPath.resolve(MetricDataWriter.FILENAME).toFile()
-        println("Writing metric data to \"${outputFile.absolutePath}\".")
-        MetricDataWriter.output(outputFile, selectedMetrics, cliState)
     }
 
 }
