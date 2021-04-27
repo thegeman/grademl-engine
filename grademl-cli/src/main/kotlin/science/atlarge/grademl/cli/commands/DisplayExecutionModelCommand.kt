6a0010b3f31cf9d3d85a3393942ba3bf6c06373f
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-04-27 19:02:18 +0200

    Add option to display-execution-model for concise output

--- grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/DisplayExecutionModelCommand.kt
+++ grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/DisplayExecutionModelCommand.kt
@@ -1,19 +1,40 @@
 package science.atlarge.grademl.cli.commands
 
 import science.atlarge.grademl.cli.CliState
+import science.atlarge.grademl.cli.util.Option
 import science.atlarge.grademl.cli.util.ParsedCommand
+import science.atlarge.grademl.core.execution.ExecutionModel
 import science.atlarge.grademl.core.execution.ExecutionPhase
 
 object DisplayExecutionModelCommand : Command(
     name = "display-execution-model",
     shortHelpMessage = "display detailed information about a job's execution model",
     longHelpMessage = "Displays information about a job's execution model. By default outputs a tree of execution phases,\n" +
-            "and for each execution phase its start time, end time, and a list of outgoing dataflows."
+            "and for each execution phase its start time, end time, and a list of outgoing dataflows.",
+    supportedOptions = listOf(
+        Option('s', "short", "only display execution phase names")
+    )
 ) {
 
     override fun process(parsedCommand: ParsedCommand, cliState: CliState) {
         println("Execution model extracted from job logs:")
+        if (parsedCommand.isOptionProvided("short")) printShort(cliState.executionModel)
+        else printLong(cliState.executionModel)
+    }
 
+    private fun printShort(executionModel: ExecutionModel) {
+        fun printPhase(phase: ExecutionPhase, indent: String) {
+            println("$indent/${phase.identifier}")
+            for (childPhase in phase.children.sortedBy { it.identifier }) {
+                printPhase(childPhase, "$indent  ")
+            }
+        }
+        for (rootPhase in executionModel.rootPhases.sortedBy { it.identifier }) {
+            printPhase(rootPhase, "  ")
+        }
+    }
+
+    private fun printLong(executionModel: ExecutionModel) {
         fun printPhase(phase: ExecutionPhase, indent: String) {
             val outFlows = phase.outFlows.sortedBy { it.identifier }
             println("$indent/${phase.identifier}")
@@ -30,7 +51,7 @@ object DisplayExecutionModelCommand : Command(
                 printPhase(childPhase, "$indent  ")
             }
         }
-        for (rootPhase in cliState.executionModel.rootPhases.sortedBy { it.identifier }) {
+        for (rootPhase in executionModel.rootPhases.sortedBy { it.identifier }) {
             printPhase(rootPhase, "  ")
         }
     }
