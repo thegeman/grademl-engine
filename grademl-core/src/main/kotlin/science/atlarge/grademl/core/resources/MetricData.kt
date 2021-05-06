9c56a56ccdf92a18ba96eeb90f16558eaf54c50a
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-07 00:44:04 +0200

    Add iterator for metric data

--- grademl-core/src/main/kotlin/science/atlarge/grademl/core/resources/MetricData.kt
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/resources/MetricData.kt
@@ -24,4 +24,77 @@ class MetricData(
 
         return MetricData(selectedTimestamps, selectedValues, maxValue)
     }
+
+    fun iterator(): MetricDataIterator {
+        return MetricDataIteratorImpl(timestamps, values)
+    }
+
+    fun iteratorFrom(startTime: TimestampNs): MetricDataIterator {
+        var startIdx = timestamps.binarySearch(startTime)
+        if (startIdx < 0) startIdx = maxOf(startIdx.inv() - 1, -1)
+        return MetricDataIteratorImpl(timestamps, values, startIdx)
+    }
+
+}
+
+interface MetricDataIterator {
+    val currentStartTime: TimestampNs
+    val currentEndTime: TimestampNs
+    val currentValue: Double
+    val hasNext: Boolean
+
+    fun peekNextEndTime(): TimestampNs
+    fun peekNextValue(): Double
+    fun next()
+}
+
+private class MetricDataIteratorImpl(
+    private val timestamps: TimestampNsArray,
+    private val values: DoubleArray,
+    initialIndex: Int = -1
+) : MetricDataIterator {
+
+    override var currentStartTime: TimestampNs = Long.MIN_VALUE
+        private set
+    override var currentEndTime: TimestampNs = timestamps.first()
+        private set
+    override var currentValue: Double = 0.0
+        private set
+
+    private var nextIndex = 0
+
+    override val hasNext: Boolean
+        get() = nextIndex < values.size
+
+    init {
+        if (initialIndex >= 0) {
+            nextIndex = minOf(initialIndex + 1, values.size)
+            currentStartTime = timestamps[nextIndex - 1]
+            currentEndTime = timestamps[nextIndex]
+            currentValue = values[nextIndex - 1]
+        }
+    }
+
+    override fun peekNextEndTime(): TimestampNs {
+        return if (hasNext) timestamps[nextIndex + 1] else Long.MAX_VALUE
+    }
+
+    override fun peekNextValue(): Double {
+        return if (hasNext) values[nextIndex] else 0.0
+    }
+
+    override fun next() {
+        if (hasNext) {
+            currentStartTime = currentEndTime
+            currentEndTime = timestamps[nextIndex + 1]
+            currentValue = values[nextIndex]
+            nextIndex++
+        } else {
+            currentStartTime = timestamps.last()
+            currentEndTime = Long.MAX_VALUE
+            currentValue = 0.0
+            nextIndex = values.size
+        }
+    }
+
 }
