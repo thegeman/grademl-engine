11d52a0d001bbfc1d39a9105add21c8a5e04977c
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-06 17:38:19 +0200

    Update top-level Resource Monitor parser to use new multi-directory parsers

--- grademl-input/grademl-input-resource-monitor/src/main/kotlin/science/atlarge/grademl/input/resource_monitor/ResourceMonitorParser.kt
+++ grademl-input/grademl-input-resource-monitor/src/main/kotlin/science/atlarge/grademl/input/resource_monitor/ResourceMonitorParser.kt
@@ -7,66 +7,42 @@ class ResourceMonitorParser private constructor(
     private val resourceMonitorMetricDirectories: Iterable<Path>
 ) {
 
-    // TODO: Support analysis of Airflow logs split over multiple directories
-
     private val hostnames = mutableSetOf<String>()
     private val cpuUtilizationData = mutableMapOf<String, CpuUtilizationData>()
     private val networkUtilizationData = mutableMapOf<String, NetworkUtilizationData>()
     private val diskUtilizationData = mutableMapOf<String, DiskUtilizationData>()
 
     private fun parse(): ResourceMonitorMetrics {
-        findHostnames()
-        parseCpuUtilization()
-        parseNetworkUtilization()
-        parseDiskUtilization()
+        parseHostnames()
+        parseMetricFiles("proc-stat", ProcStatParser, cpuUtilizationData)
+        parseMetricFiles("proc-net-dev", ProcNetDevParser, networkUtilizationData)
+        parseMetricFiles("proc-diskstats", ProcDiskstatsParser, diskUtilizationData)
         return ResourceMonitorMetrics(hostnames, cpuUtilizationData, networkUtilizationData, diskUtilizationData)
     }
 
-    private fun findHostnames() {
+    private fun parseHostnames() {
         // Enumerate metric files and extract hostnames from the filenames
-        resourceMonitorMetricDirectories.first().toFile()
-            .walk()
-            .filter { it.isFile && "-" in it.name }
-            .map { it.name.split("-").last() }
-            .toCollection(hostnames)
-    }
-
-    private fun parseCpuUtilization() {
-        // Find relevant CPU metric files
-        val cpuMetricFiles = resourceMonitorMetricDirectories.first().toFile()
-            .walk()
-            .filter { it.isFile && it.name.startsWith("proc-stat") }
-        // Parse each file and associate it with a hostname
-        val parser = ProcStatParser()
-        for (cpuMetricFile in cpuMetricFiles) {
-            val hostname = cpuMetricFile.name.split("-").last()
-            cpuUtilizationData[hostname] = parser.parse(cpuMetricFile)
+        for (directory in resourceMonitorMetricDirectories) {
+            directory.toFile()
+                .walk()
+                .filter { it.isFile && "-" in it.name }
+                .map { it.name.split("-").last() }
+                .toCollection(hostnames)
         }
     }
 
-    private fun parseNetworkUtilization() {
-        // Find relevant network metric files
-        val networkMetricFiles = resourceMonitorMetricDirectories.first().toFile()
+    private fun <T> parseMetricFiles(filePrefix: String, parser: FileParser<T>, outMap: MutableMap<String, T>) {
+        // Find relevant metric files
+        val allMetricFiles = resourceMonitorMetricDirectories.first().toFile()
             .walk()
-            .filter { it.isFile && it.name.startsWith("proc-net-dev") }
-        // Parse each file and associate it with a hostname
-        val parser = ProcNetDevParser()
-        for (networkMetricFile in networkMetricFiles) {
-            val hostname = networkMetricFile.name.split("-").last()
-            networkUtilizationData[hostname] = parser.parse(networkMetricFile)
+            .filter { it.isFile && it.name.startsWith(filePrefix) }
+        // Group files by hostname
+        val metricFilesByHostname = allMetricFiles.groupBy {
+            it.name.split("-").last()
         }
-    }
-
-    private fun parseDiskUtilization() {
-        // Find relevant disk metric files
-        val diskMetricFiles = resourceMonitorMetricDirectories.first().toFile()
-            .walk()
-            .filter { it.isFile && it.name.startsWith("proc-diskstats") }
-        // Parse each file and associate it with a hostname
-        val parser = ProcDiskstatsParser()
-        for (diskMetricFile in diskMetricFiles) {
-            val hostname = diskMetricFile.name.split("-").last()
-            diskUtilizationData[hostname] = parser.parse(diskMetricFile)
+        // Parse all metric files to produce one data structure per hostname
+        for ((hostname, metricFiles) in metricFilesByHostname) {
+            outMap[hostname] = parser.parse(hostname, metricFiles)
         }
     }
 
