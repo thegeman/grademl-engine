36a7ceb421e64da05f5db03d2f28fbac6027bd5b
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-06 18:20:20 +0200

    Add utility function for concatenating optional arrays

--- grademl-input/grademl-input-resource-monitor/src/main/kotlin/science/atlarge/grademl/input/resource_monitor/util/PrimitiveArrays.kt
+++ grademl-input/grademl-input-resource-monitor/src/main/kotlin/science/atlarge/grademl/input/resource_monitor/util/PrimitiveArrays.kt
@@ -47,3 +47,44 @@ fun concatenateArrays(arrays: List<DoubleArray>, separator: DoubleArray = double
 
     return outArray
 }
+
+fun concatenateOptionalArrays(
+    arrays: List<DoubleArray?>,
+    expectedArraySizes: List<Int>,
+    separator: DoubleArray = doubleArrayOf()
+): DoubleArray? {
+    require(arrays.indices.all { i -> arrays[i] == null || arrays[i]!!.size == expectedArraySizes[i] }) {
+        "Given arrays must match the expected array sizes"
+    }
+    // Skip if no arrays are provided
+    if (arrays.isEmpty()) {
+        return doubleArrayOf()
+    }
+    if (arrays.all { it == null }) {
+        return null
+    }
+    // Allocate an array of the correct dimensions
+    val totalSize = expectedArraySizes.sum() + separator.size * (arrays.size - 1)
+    val outArray = DoubleArray(totalSize)
+    // Iterate over arrays and copy their contents to the output array, replacing null arrays with zero values,
+    // and inserting separators as appropriate
+    var offset = 0
+    for (i in arrays.indices) {
+        // Insert separator, except before the first array
+        if (i != 0) {
+            separator.copyInto(outArray, destinationOffset = offset)
+            offset += separator.size
+        }
+        // Copy next array, if it exists
+        if (arrays[i] != null) {
+            arrays[i]!!.copyInto(outArray, destinationOffset = offset)
+            offset += arrays[i]!!.size
+        } else {
+            // Otherwise, insert zero values
+            outArray.fill(0.0, offset, offset + expectedArraySizes[i])
+            offset += expectedArraySizes[i]
+        }
+    }
+
+    return outArray
+}
