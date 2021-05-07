ddfa77b9692460a51ce65dc58993c3ca5089adf3
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-07 02:21:07 +0200

    Add input parsing to GradeMLJob

--- grademl-core/src/main/kotlin/science/atlarge/grademl/core/GradeMLEngine.kt
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/GradeMLEngine.kt
@@ -1,11 +1,18 @@
 package science.atlarge.grademl.core
 
+import science.atlarge.grademl.core.input.InputSource
 import java.nio.file.Path
 
 object GradeMLEngine {
 
+    private val knownInputSources = mutableSetOf<InputSource>()
+
     fun analyzeJob(jobDataDirectories: Iterable<Path>, jobOutputDirectory: Path): GradeMLJob {
-        return GradeMLJob(jobOutputDirectory)
+        return GradeMLJob(jobDataDirectories, jobOutputDirectory, knownInputSources)
+    }
+
+    fun registerInputSource(inputSource: InputSource) {
+        knownInputSources.add(inputSource)
     }
 
 }
--- grademl-core/src/main/kotlin/science/atlarge/grademl/core/GradeMLJob.kt
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/GradeMLJob.kt
@@ -1,11 +1,28 @@
 package science.atlarge.grademl.core
 
+import science.atlarge.grademl.core.execution.ExecutionModel
+import science.atlarge.grademl.core.input.InputSource
+import science.atlarge.grademl.core.resources.ResourceModel
 import java.nio.file.Path
 
 class GradeMLJob(
-    val outputDirectory: Path
+    inputDirectories: Iterable<Path>,
+    outputDirectory: Path,
+    inputSources: Iterable<InputSource>
 ) {
 
+    private val unifiedModels by lazy {
+        val executionModel = ExecutionModel()
+        val resourceModel = ResourceModel()
+        for (inputSource in inputSources) {
+            inputSource.parseJobData(inputDirectories, executionModel, resourceModel)
+        }
+        (executionModel to resourceModel)
+    }
 
+    val unifiedExecutionModel: ExecutionModel
+        get() = unifiedModels.first
+    val unifiedResourceModel: ResourceModel
+        get() = unifiedModels.second
 
 }
