45fb2b2cbeeaeb6af92b5378a28022c60d42f770
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-07 13:15:45 +0200

    Add skeleton R script for plot-metric command

---
+++ grademl-cli/src/main/R/plot-metric.R
@@ -0,0 +1,15 @@
+require(data.table)
+require(ggplot2)
+require(ggpubr)
+
+phase_list_filename <- "phase-list.tsv"
+metric_data_filename <- "metric-data.tsv"
+upsampled_metric_data_filename <- "upsampled-metric-data.tsv"
+resource_attribution_data_filename <- "resource-attribution-data.tsv"
+
+### START OF GENERATED SETTINGS ###
+### :setting output_directory   ###
+### :setting data_directory     ###
+### :setting plot_filename      ###
+### :setting metric_id          ###
+### END OF GENERATED SETTINGS   ###
