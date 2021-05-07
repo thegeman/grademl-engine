2f2cc65677d2f88966c0f1e49f75e9afaf7f5846
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-07 13:14:50 +0200

    Add resource attribution data writer for plotting the outcome of resource attribution

---
+++ grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/data/ResourceAttributionDataWriter.kt
@@ -0,0 +1,65 @@
+package science.atlarge.grademl.cli.data
+
+import science.atlarge.grademl.cli.CliState
+import science.atlarge.grademl.core.TimestampNs
+import science.atlarge.grademl.core.execution.ExecutionPhase
+import science.atlarge.grademl.core.resources.Metric
+import science.atlarge.grademl.core.resources.MetricData
+import java.io.File
+
+object ResourceAttributionDataWriter {
+
+    const val FILENAME = "resource-attribution-data.tsv"
+
+    fun output(
+        outFile: File,
+        attributionData: Map<Metric, Map<ExecutionPhase, MetricData>>,
+        cliState: CliState
+    ) {
+        outFile.bufferedWriter().use { writer ->
+            writer.appendLine("metric.id\tphase.id\ttimestamp\tvalue")
+            val metricsById = attributionData.map { (metric, phases) ->
+                cliState.metricList.metricToIdentifier(metric) to phases
+            }.sortedBy { it.first }
+            for ((metricId, phases) in metricsById) {
+                val phasesById = phases.map { (phase, data) ->
+                    cliState.phaseList.phaseToIdentifier(phase) to data
+                }.sortedBy { it.first }
+                for ((phaseId, phaseData) in phasesById) {
+                    fun printLine(timestamp: TimestampNs, value: String) {
+                        writer.apply {
+                            append(metricId)
+                            append('\t')
+                            append(phaseId)
+                            append('\t')
+                            append(cliState.normalizeTimestamp(timestamp).toString())
+                            append('\t')
+                            appendLine(value)
+                        }
+                    }
+
+                    // Skip metrics without data points
+                    if (phaseData.timestamps.size < 2) {
+                        continue
+                    }
+
+                    val timestamps = phaseData.timestamps
+                    printLine(timestamps[0], "0")
+                    val values = phaseData.values
+                    var lastValue = 0.0
+                    for (index in values.indices) {
+                        val newValue = values[index]
+                        if (newValue != lastValue) {
+                            // Only output a data point whenever the metric's value changes
+                            if (index != 0) printLine(timestamps[index], lastValue.toString())
+                            lastValue = newValue
+                        }
+                    }
+                    // Always output a data point at the end of a time series
+                    printLine(timestamps.last(), lastValue.toString())
+                }
+            }
+        }
+    }
+
+}
