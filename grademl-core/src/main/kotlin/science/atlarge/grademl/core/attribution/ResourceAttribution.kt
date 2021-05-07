088a0378225c9e4a5b60fc0556be33b7afbe8c4e
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-07 02:12:29 +0200

    Add wrapper class for resource attribution process

---
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/attribution/ResourceAttribution.kt
@@ -0,0 +1,48 @@
+package science.atlarge.grademl.core.attribution
+
+import science.atlarge.grademl.core.execution.ExecutionModel
+import science.atlarge.grademl.core.execution.ExecutionPhase
+import science.atlarge.grademl.core.resources.Metric
+import science.atlarge.grademl.core.resources.MetricData
+import science.atlarge.grademl.core.resources.ResourceModel
+
+class ResourceAttribution(
+    executionModel: ExecutionModel,
+    resourceModel: ResourceModel,
+    attributionRuleProvider: ResourceAttributionRuleProvider
+) {
+
+    private val leafPhases = executionModel.rootPhase.phasesInTree.filter { it.children.isEmpty() }.toSet()
+    private val metrics = resourceModel.rootResource.metricsInTree
+
+    private val demandEstimationStep = ResourceDemandEstimationStep(
+        metrics,
+        leafPhases,
+        attributionRuleProvider
+    )
+
+    private val upsamplingStep = ResourceUpsamplingStep(
+        metrics
+    ) { metric -> demandEstimationStep.estimatedDemandForMetric(metric)!! }
+
+    private val attributionStep = ResourceAttributionStep(
+        leafPhases,
+        metrics,
+        attributionRuleProvider,
+        { metric -> demandEstimationStep.estimatedDemandForMetric(metric)!! },
+        { metric -> upsamplingStep.upsampleMetric(metric)!! }
+    )
+
+    fun attributeMetricToPhase(metric: Metric, phase: ExecutionPhase): MetricData? {
+        return attributionStep.attributeMetricToPhase(metric, phase)
+    }
+
+    fun upsampleMetric(metric: Metric): MetricData? {
+        return upsamplingStep.upsampleMetric(metric)
+    }
+
+    fun estimateDemand(metric: Metric): ResourceDemandEstimate? {
+        return demandEstimationStep.estimatedDemandForMetric(metric)
+    }
+
+}
