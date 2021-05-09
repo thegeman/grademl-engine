7f80f6940305697443d8e96911db36bf5c479927
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-09 17:19:54 +0200

    Add log file discovery to SparkLogParser

--- grademl-input/grademl-input-spark/src/main/kotlin/science/atlarge/grademl/input/spark/SparkLogParser.kt
+++ grademl-input/grademl-input-spark/src/main/kotlin/science/atlarge/grademl/input/spark/SparkLogParser.kt
@@ -1,13 +1,51 @@
 package science.atlarge.grademl.input.spark
 
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import java.io.File
+import java.nio.file.Files
 import java.nio.file.Path
+import kotlin.streams.toList
 
 class SparkLogParser private constructor(
     private val sparkLogDirectories: Iterable<Path>
 ) {
 
+    private val appLogFiles = mutableSetOf<File>()
+
     private fun parse(): SparkLog {
+        findAppLogFiles()
         return SparkLog()
+    }
+
+    private fun findAppLogFiles() {
+        // Find all files in the Spark log directory
+        val logFiles = sparkLogDirectories.flatMap { directory ->
+            Files.list(directory).use { fileList ->
+                fileList.map { it.toFile() }
+                    .filter { it.isFile }
+                    .toList()
+            }
+        }
+        // Check each file for a Spark application ID
+        for (logFile in logFiles) {
+            // Find the first ApplicationStart event, if it exists in this file, to verify that this file is
+            // a Spark application log file
+            val applicationStartLine = logFile.useLines { lines ->
+                lines.firstOrNull { "SparkListenerApplicationStart" in it }
+            } ?: continue
+            try {
+                val eventJson = Json.parseToJsonElement(applicationStartLine)
+                if (eventJson !is JsonObject) continue
+                val appIdJson = eventJson["App ID"] ?: continue
+                if (appIdJson !is JsonPrimitive) continue
+
+                appLogFiles.add(logFile)
+            } catch (_: Exception) {
+                continue
+            }
+        }
     }
 
     companion object {
