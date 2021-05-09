1f475f9a08dadde0a9c35cc8250e5396e63db1a8
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-09 16:11:53 +0200

    Add module for Spark log parsing

---
+++ grademl-input/grademl-input-spark/build.gradle.kts
@@ -0,0 +1,9 @@
+description = "Parsing for Spark log files"
+
+plugins {
+    kotlin("jvm") version "1.5.0-RC"
+}
+
+dependencies {
+    implementation(project(":grademl-core"))
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,4 +3,5 @@ rootProject.name = "grademl-engine"
 include(":grademl-core")
 include(":grademl-input:grademl-input-airflow")
 include(":grademl-input:grademl-input-resource-monitor")
+include(":grademl-input:grademl-input-spark")
 include(":grademl-cli")
