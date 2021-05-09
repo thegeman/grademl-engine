091fb61c6b9cc22df73cd708ec6759a8e3240fca
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-09 16:51:16 +0200

    Add main function for manual testing of Spark log parser

--- grademl-input/grademl-input-spark/src/main/kotlin/science/atlarge/grademl/input/spark/Spark.kt
+++ grademl-input/grademl-input-spark/src/main/kotlin/science/atlarge/grademl/input/spark/Spark.kt
@@ -1,9 +1,12 @@
 package science.atlarge.grademl.input.spark
 
 import science.atlarge.grademl.core.execution.ExecutionModel
+import science.atlarge.grademl.core.execution.ExecutionPhase
 import science.atlarge.grademl.core.input.InputSource
 import science.atlarge.grademl.core.resources.ResourceModel
 import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.system.exitProcess
 
 object Spark : InputSource {
 
@@ -25,4 +28,39 @@ object Spark : InputSource {
         return true
     }
 
+}
+
+// Wrapper for testing the log parser
+fun main(args: Array<String>) {
+    if (args.isEmpty() || args[0] == "--help") {
+        println("Arguments: <jobLogDirectory> [...]")
+        exitProcess(if (args.isEmpty()) -1 else 0)
+    }
+
+    val executionModel = ExecutionModel()
+    val foundSparkLogs = Spark.parseJobData(args.map { Paths.get(it) }, executionModel, ResourceModel())
+    require(foundSparkLogs) {
+        "Cannot find Spark logs in any of the given jobLogDirectories"
+    }
+    println("Execution model extracted from Spark logs:")
+
+    fun printPhase(phase: ExecutionPhase, indent: String) {
+        val outFlows = phase.outFlows.sortedBy { it.identifier }
+        println("$indent/${phase.identifier}")
+        println(
+            "$indent      Start time:          %d.%09d"
+                .format(phase.startTime / 1_000_000_000, phase.startTime % 1_000_000_000)
+        )
+        println(
+            "$indent      End time:            %d.%09d"
+                .format(phase.endTime / 1_000_000_000, phase.endTime % 1_000_000_000)
+        )
+        println("$indent      Outgoing dataflows:  (${outFlows.joinToString(", ") { it.identifier }})")
+        for (childPhase in phase.children.sortedBy { it.identifier }) {
+            printPhase(childPhase, "$indent  ")
+        }
+    }
+    for (topLevelPhase in executionModel.rootPhase.children.sortedBy { it.identifier }) {
+        printPhase(topLevelPhase, "  ")
+    }
 }
