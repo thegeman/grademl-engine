80751d2b31ab2773f86919b5c300e37d13406348
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-10 13:08:18 +0200

    Change best-fit attribution rule to use phase types instead of individual phases for efficiency

--- grademl-core/src/main/kotlin/science/atlarge/grademl/core/attribution/BestFitAttributionRuleProvider.kt
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/attribution/BestFitAttributionRuleProvider.kt
@@ -15,7 +15,8 @@ class BestFitAttributionRuleProvider(
 ) : ResourceAttributionRuleProvider {
 
     private val phases = phases.toSet()
-    private val orderedPhaseList = phases.sortedBy { it.path }
+    private val phasesByType = phases.groupBy { it.typePath }
+    private val orderedPhaseTypes = phasesByType.keys.sorted()
     private val metrics = metrics.toSet()
 
     private val cachedRules = mutableMapOf<Metric, Map<ExecutionPhase, ResourceAttributionRule>>()
@@ -24,13 +25,13 @@ class BestFitAttributionRuleProvider(
         // Sanity check the arguments
         if (phase !in phases || metric !in metrics) return ResourceAttributionRule.None
         // Return from cache if available
-        val cachedRule = cachedRules[metric].orEmpty()[phase]
-        if (cachedRule != null) {
-            return cachedRule
+        val cachedMetric = cachedRules[metric]
+        if (cachedMetric != null) {
+            return cachedMetric[phase] ?: ResourceAttributionRule.None
         }
         // Otherwise, fit attribution rules to observed resource usage and phase activity
         computeFitForMetric(metric)
-        return cachedRules[metric].orEmpty()[phase] ?: ResourceAttributionRule.None
+        return cachedRules[metric]!![phase] ?: ResourceAttributionRule.None
     }
 
     private fun computeFitForMetric(metric: Metric) {
@@ -40,13 +41,14 @@ class BestFitAttributionRuleProvider(
         // Perform the NNLS fit
         val bestFit = NonNegativeLeastSquares.fit(activityMatrix, observationVector, scratchDirectory)
         // Translate the obtained coefficients to attribution rules
-        cachedRules[metric] = orderedPhaseList.mapIndexed { i, phase ->
-            phase to if (bestFit[i] > 0.0) {
+        val rulePerPhaseType = orderedPhaseTypes.mapIndexed { i, phaseType ->
+            phaseType to if (bestFit[i] > 0.0) {
                 ResourceAttributionRule.Variable(bestFit[i])
             } else {
                 ResourceAttributionRule.None
             }
         }.toMap()
+        cachedRules[metric] = phases.associateWith { rulePerPhaseType[it.typePath]!! }
     }
 
     private fun createPhaseActivityMatrix(
@@ -56,13 +58,11 @@ class BestFitAttributionRuleProvider(
             val startTime = timestamps[timePeriodId]
             val endTime = timestamps[timePeriodId + 1]
             val periodLength = endTime - startTime
-            DoubleArray(phases.size) { phaseId ->
-                val overlap = maxOf(
-                    0,
-                    minOf(endTime, orderedPhaseList[phaseId].endTime) -
-                            maxOf(startTime, orderedPhaseList[phaseId].startTime)
-                )
-                overlap.toDouble() / periodLength
+            DoubleArray(orderedPhaseTypes.size) { phaseTypeId ->
+                val phaseType = orderedPhaseTypes[phaseTypeId]
+                phasesByType[phaseType]!!.sumOf {
+                    (minOf(endTime, it.endTime) - maxOf(startTime, it.startTime)).coerceAtLeast(0)
+                }.toDouble() / periodLength
             }
         }
     }
