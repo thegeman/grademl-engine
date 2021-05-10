935a8cf176bf8112e972029e1212d82839ea2687
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-10 19:17:54 +0200

    Update plot-metric command and plot script to support per-phase and per-phase-type resource attribution

--- grademl-cli/src/main/R/plot-metric.R
+++ grademl-cli/src/main/R/plot-metric.R
@@ -3,96 +3,61 @@ require(ggplot2)
 require(ggpubr)
 
 phase_list_filename <- "phase-list.tsv"
-metric_list_filename <- "metric-list.tsv"
+phase_type_list_filename <- "phase-type-list.tsv"
 metric_data_filename <- "metric-data.tsv"
 upsampled_metric_data_filename <- "upsampled-metric-data.tsv"
 resource_attribution_data_filename <- "resource-attribution-data.tsv"
+aggregate_resource_attribution_data_filename <- "aggregate-resource-attribution-data.tsv"
 
 ### START OF GENERATED SETTINGS ###
 ### :setting output_directory   ###
 ### :setting data_directory     ###
 ### :setting plot_filename      ###
 ### :setting metric_id          ###
+### :setting plot_per_phase     ###
 ### END OF GENERATED SETTINGS   ###
 
 ####################
 ### DATA LOADING ###
 ####################
 
-# Read phase list
-phase_list <- fread(file = file.path(data_directory, phase_list_filename))
-phase_list <- phase_list[, .(
-  phase.id = as.factor(phase.id),
-  phase.path = as.factor(phase.path),
-  parent.phase.id = as.factor(parent.phase.id),
-  depth = depth,
-  start.time = start.time.ns / 1e9,
-  end.time = end.time.ns / 1e9,
-  canonical.index
-)][, `:=`(
-  parent.phase.id = factor(parent.phase.id, levels(phase.id))
-)][order(-canonical.index)][, `:=`(
-  order.num = 0:(.N - 1)
-)]
-
-# Read metric list
-metric_list <- fread(file = file.path(data_directory, metric_list_filename))
-metric_list <- metric_list[, .(
-  metric.id = as.factor(metric.id),
-  metric.path = as.factor(metric.path),
-  max.value = as.double(max.value)
+# Read phase (type) list
+if (plot_per_phase) {
+  phase_list <- fread(file = file.path(data_directory, phase_list_filename))[, .(
+    phase.id = as.factor(phase.id),
+    phase.path = phase.path,
+    canonical.index
+  )]
+} else {
+  phase_list <- fread(file = file.path(data_directory, phase_type_list_filename))[, .(
+    phase.id = as.factor(phase.type.id),
+    phase.path = phase.type.path,
+    canonical.index
+  )]
+}
+phase_list <- phase_list[order(canonical.index)][, `:=`(
+  canonical.index = NULL,
+  phase.path = factor(phase.path, phase.path)
 )]
 
 # Read metric data
-metric_data <- fread(file = file.path(data_directory, metric_data_filename))[, .(
-  metric.id,
-  timestamp = timestamp / 1e9,
-  value
-)]
-metric_data <- merge(metric_data, metric_list, by = "metric.id")[, .(
-  metric.id,
-  metric.path,
-  timestamp,
-  value,
-  max.value
-)]
+metric_data <- fread(file = file.path(data_directory, metric_data_filename))[
+  metric.id == metric_id, .(timestamp = timestamp / 1e9, value)]
 
 # Read upsampled metric data
-upsampled_metric_data <- fread(file = file.path(data_directory, upsampled_metric_data_filename))[, .(
-  metric.id,
-  timestamp = timestamp / 1e9,
-  value
-)]
-upsampled_metric_data <- merge(upsampled_metric_data, metric_list, by = "metric.id")[, .(
-  metric.id,
-  metric.path,
-  timestamp,
-  value,
-  max.value
-)]
+upsampled_metric_data <- fread(file = file.path(data_directory, upsampled_metric_data_filename))[
+  metric.id == metric_id, .(timestamp = timestamp / 1e9, value)]
 
-# Read resource attribution data
-resource_attribution_data <- fread(file = file.path(data_directory, resource_attribution_data_filename))[, .(
-  metric.id,
-  phase.id,
-  timestamp = timestamp / 1e9,
-  value
-)]
-resource_attribution_data <- merge(resource_attribution_data, metric_list, by = "metric.id")[, .(
-  metric.id,
-  metric.path,
-  phase.id,
-  timestamp,
-  value
-)]
-resource_attribution_data <- merge(resource_attribution_data, phase_list, by = "phase.id")[, .(
-  metric.id,
-  metric.path,
-  phase.id,
-  phase.path,
-  timestamp,
-  value
-)]
+# Read (aggregated) resource attribution data
+if (plot_per_phase) {
+  resource_attribution_data <- fread(file = file.path(data_directory, resource_attribution_data_filename))[
+    metric.id == metric_id, .(phase.id, timestamp = timestamp / 1e9, value, count = 1)]
+} else {
+  resource_attribution_data <- fread(file = file.path(data_directory, aggregate_resource_attribution_data_filename))[
+    metric.id == metric_id, .(phase.id = phase.type.id, timestamp = timestamp / 1e9, value, count)]
+}
+resource_attribution_data <- merge(resource_attribution_data, phase_list, by = "phase.id")[,
+  .(phase.path, timestamp, value, count)]
 
 ##########################
 ### DATA PREPROCESSING ###
@@ -102,17 +67,18 @@ resource_attribution_data <- merge(resource_attribution_data, phase_list, by = "
 stepped_metric_data <- metric_data[, .(
   timestamp = c(head(timestamp, -1) + 1e-9, tail(timestamp, -1)),
   value = c(tail(value, -1), tail(value, -1))
-), by = .(metric.id, metric.path, max.value)][order(metric.id, timestamp)]
+)][order(timestamp)]
 
 stepped_upsampled_metric_data <- upsampled_metric_data[, .(
   timestamp = c(head(timestamp, -1) + 1e-9, tail(timestamp, -1)),
   value = c(tail(value, -1), tail(value, -1))
-), by = .(metric.id, metric.path, max.value)][order(metric.id, timestamp)]
+)][order(timestamp)]
 
 stepped_resource_attribution_data <- resource_attribution_data[, .(
   timestamp = c(head(timestamp, -1) + 1e-9, tail(timestamp, -1)),
-  value = c(tail(value, -1), tail(value, -1))
-), by = .(metric.id, metric.path, phase.id, phase.path)][order(metric.id, phase.id, timestamp)]
+  value = c(tail(value, -1), tail(value, -1)),
+  count = c(tail(count, -1), tail(count, -1))
+), by = .(phase.path)][order(phase.path, timestamp)]
 
 ################
 ### PLOTTING ###
@@ -139,7 +105,7 @@ p_upsampled_metric <- ggplot(stepped_upsampled_metric_data) +
   theme_bw()
 
 # Plot resource attribution data
-phase_count <- length(unique(stepped_resource_attribution_data$phase.id))
+phase_count <- length(unique(stepped_resource_attribution_data$phase.path))
 p_resource_attribution <- ggplot(stepped_resource_attribution_data) +
   geom_line(data = stepped_upsampled_metric_data, aes(x = timestamp, y = value),
             colour = "gray70", size = 0.05) +
--- grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/PlotMetricCommand.kt
+++ grademl-cli/src/main/kotlin/science/atlarge/grademl/cli/commands/PlotMetricCommand.kt
@@ -1,12 +1,10 @@
 package science.atlarge.grademl.cli.commands
 
 import science.atlarge.grademl.cli.CliState
-import science.atlarge.grademl.cli.data.MetricDataWriter
-import science.atlarge.grademl.cli.data.MetricListWriter
-import science.atlarge.grademl.cli.data.PhaseListWriter
-import science.atlarge.grademl.cli.data.ResourceAttributionDataWriter
+import science.atlarge.grademl.cli.data.*
 import science.atlarge.grademl.cli.terminal.Argument
 import science.atlarge.grademl.cli.terminal.ArgumentValueConstraint
+import science.atlarge.grademl.cli.terminal.Option
 import science.atlarge.grademl.cli.terminal.ParsedCommand
 import science.atlarge.grademl.cli.util.parseMetricPathExpression
 import science.atlarge.grademl.cli.util.tryMatchMetricPath
@@ -21,6 +19,13 @@ object PlotMetricCommand : Command(
     shortHelpMessage = "plot a comprehensive view of a particular metric",
     longHelpMessage = "Plots a metric's observed time series, its upsampled time series, " +
             "and its attribution to phases.",
+    supportedOptions = listOf(
+        Option(
+            null,
+            "show-phases",
+            "show phases instead of phase types in plot"
+        )
+    ),
     supportedArguments = listOf(
         Argument(
             "metric_path",
@@ -36,6 +41,9 @@ object PlotMetricCommand : Command(
     private const val SCRIPT_FILENAME = "plot-metric.R"
 
     override fun process(parsedCommand: ParsedCommand, cliState: CliState) {
+        // Parse options
+        val showPhases = parsedCommand.isOptionProvided("show-phases")
+
         // Parse the metric_path argument value(s) for a set of metrics to plot
         val metrics = parsedCommand.getArgumentValues("metric_path")
             .map { parseMetricPathExpression(it) ?: return }
@@ -52,13 +60,14 @@ object PlotMetricCommand : Command(
 
         // Plot each metric
         for (metric in metrics) {
-            plotMetric(metric, resourceAttribution, cliState)
+            plotMetric(metric, resourceAttribution, showPhases, cliState)
         }
     }
 
     private fun plotMetric(
         metric: Metric,
         resourceAttribution: ResourceAttribution,
+        showPhases: Boolean,
         cliState: CliState
     ) {
         // Create output paths for data and scripts
@@ -72,34 +81,31 @@ object PlotMetricCommand : Command(
             resourceAttribution.attributeMetricToPhase(metric, it)!!
         }
 
-        // Output list of leaf phases
-        val phaseListFile = dataOutputDirectory.resolve(PhaseListWriter.FILENAME).toFile()
-        println("Writing list of leaf phases to \"${phaseListFile.absolutePath}\".")
+        // Output list of leaf phases, if needed
         val selectedPhases = resourceAttribution.leafPhases
-        PhaseListWriter.output(
-            phaseListFile,
-            cliState.executionModel.rootPhase,
-            selectedPhases,
-            cliState
-        )
+        val phaseListFile = dataOutputDirectory.resolve(PhaseListWriter.FILENAME).toFile()
+        if (showPhases) {
+            println("Writing list of leaf phases to \"${phaseListFile.absolutePath}\".")
+            PhaseListWriter.output(phaseListFile, cliState.executionModel.rootPhase, selectedPhases, cliState)
+        }
+
+        // Output list of leaf phase types, if needed
+        val selectedPhaseTypes = selectedPhases.map { it.type }.toSet()
+        val phaseTypeListFile = dataOutputDirectory.resolve(PhaseTypeListWriter.FILENAME).toFile()
+        if (!showPhases) {
+            println("Writing list of leaf phase types to \"${phaseTypeListFile.absolutePath}\".")
+            PhaseTypeListWriter.output(phaseTypeListFile, selectedPhaseTypes, cliState)
+        }
 
         // Output list of metrics
         val metricListFile = dataOutputDirectory.resolve(MetricListWriter.FILENAME).toFile()
         println("Writing list of metrics to \"${metricListFile.absolutePath}\".")
-        MetricListWriter.output(
-            metricListFile,
-            listOf(metric),
-            cliState
-        )
+        MetricListWriter.output(metricListFile, listOf(metric), cliState)
 
         // Write raw metric data
         val metricDataFile = dataOutputDirectory.resolve(MetricDataWriter.FILENAME).toFile()
         println("Writing observed metric data to \"${metricDataFile.absolutePath}\".")
-        MetricDataWriter.output(
-            metricDataFile,
-            listOf(metric),
-            cliState
-        )
+        MetricDataWriter.output(metricDataFile, listOf(metric), cliState)
 
         // Write upsampled metric data
         val upsampledMetricDataFile = dataOutputDirectory.resolve("upsampled-metric-data.tsv").toFile()
@@ -111,14 +117,28 @@ object PlotMetricCommand : Command(
             metricDataSelector = { resourceAttribution.upsampleMetric(it)!! }
         )
 
-        // Write attributed resource usage
+        // Write attributed resource usage per phase, if needed
         val resourceAttributionDataFile = dataOutputDirectory.resolve(ResourceAttributionDataWriter.FILENAME).toFile()
-        println("Writing resource attribution data to \"${resourceAttributionDataFile.absolutePath}\".")
-        ResourceAttributionDataWriter.output(
-            resourceAttributionDataFile,
-            mapOf(metric to phaseAttribution),
-            cliState
-        )
+        if (showPhases) {
+            println("Writing resource attribution data to \"${resourceAttributionDataFile.absolutePath}\".")
+            ResourceAttributionDataWriter.output(
+                resourceAttributionDataFile,
+                mapOf(metric to phaseAttribution),
+                cliState
+            )
+        }
+
+        // Write attributed resource usage per phase type, if needed
+        val aggregatedResourceAttributionDataFile = dataOutputDirectory
+            .resolve(AggregateResourceAttributionDataWriter.FILENAME).toFile()
+        if (!showPhases) {
+            println("Writing resource attribution data to \"${aggregatedResourceAttributionDataFile.absolutePath}\".")
+            AggregateResourceAttributionDataWriter.output(
+                aggregatedResourceAttributionDataFile,
+                mapOf(metric to phaseAttribution),
+                cliState
+            )
+        }
 
         // Instantiate the plot script template
         val rScriptFile = rScriptDirectory.resolve(SCRIPT_FILENAME).toFile()
@@ -129,7 +149,8 @@ object PlotMetricCommand : Command(
                 "plot_filename" to "\"${plotOutputFile.name}\"",
                 "output_directory" to metricOutputDirectory.toFile().asRPathString(),
                 "data_directory" to dataOutputDirectory.toFile().asRPathString(),
-                "metric_id" to "\"${cliState.metricList.metricToIdentifier(metric)}\""
+                "metric_id" to "\"${cliState.metricList.metricToIdentifier(metric)}\"",
+                "plot_per_phase" to if (showPhases) "TRUE" else "FALSE"
             )
         )
 
