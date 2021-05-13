dacfcd3dd5fd3f01830fe05c82942e0a8040d423
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-05-13 14:39:14 +0200

    Add Environment class to hold metadata about a job's execution environment

---
+++ grademl-core/src/main/kotlin/science/atlarge/grademl/core/models/Environment.kt
@@ -0,0 +1,26 @@
+package science.atlarge.grademl.core.models
+
+class Environment {
+
+    private val _machines = mutableListOf<Machine>()
+    private val machinesById = mutableMapOf<String, Machine>()
+
+    val machines: List<Machine>
+        get() = _machines
+
+    fun machineForId(id: String): Machine? = machinesById[id]
+
+    fun addMachine(machine: Machine) {
+        require(machine.canonicalId !in machinesById && machine.alternativeIds.none { it in machinesById }) {
+            "Cannot add machine with an ID already in use by a different machine"
+        }
+        // Keep machine list sorted by canonical ID
+        val positionToInsert = _machines.binarySearch(machine, compareBy(Machine::canonicalId)).let { idx ->
+            if (idx < 0) idx.inv() else idx
+        }
+        _machines.add(positionToInsert, machine)
+    }
+
+}
+
+class Machine(val canonicalId: String, val alternativeIds: Set<String>)
