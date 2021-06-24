247d40b2dc184f4bda6afa2adba5c1b29482734b
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-06-24 16:20:24 +0200

    Add submodule for query engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    java
+}
+
 allprojects {
     group = "science.atlarge.grademl"
     version = "0.1-SNAPSHOT"
@@ -5,4 +9,8 @@ allprojects {
     repositories {
         mavenCentral()
     }
+
+    apply(plugin = "java")
+    java.sourceCompatibility = JavaVersion.VERSION_11
+    java.targetCompatibility = JavaVersion.VERSION_11
 }
---
+++ grademl-query/build.gradle.kts
@@ -0,0 +1,20 @@
+description = "Query engine for GradeML"
+
+plugins {
+    kotlin("jvm") version "1.5.0"
+    application
+}
+
+application {
+    mainClass.set("science.atlarge.grademl.query.QueryCli")
+}
+
+dependencies {
+    implementation(project(":grademl-core"))
+    implementation(project(":grademl-input:grademl-input-airflow"))
+    implementation(project(":grademl-input:grademl-input-resource-monitor"))
+    implementation(project(":grademl-input:grademl-input-spark"))
+
+    implementation("org.jline:jline:3.19.0")
+    runtimeOnly("org.fusesource.jansi:jansi:2.3.2")
+}
