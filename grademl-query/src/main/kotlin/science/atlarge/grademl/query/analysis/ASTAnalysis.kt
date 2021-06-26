945f7103b34109af9ec9d32acbb82babfe4d9b4f
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-06-26 17:14:15 +0200

    Encapsulate AST analysis passes in a single function

---
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/analysis/ASTAnalysis.kt
@@ -0,0 +1,14 @@
+package science.atlarge.grademl.query.analysis
+
+import science.atlarge.grademl.query.language.Expression
+import science.atlarge.grademl.query.model.Column
+
+object ASTAnalysis {
+
+    fun analyzeExpression(expression: Expression, columns: List<Column>): Expression {
+        ColumnResolution.resolveColumns(expression, columns)
+        TypeChecking.analyzeTypes(expression, columns)
+        return expression
+    }
+
+}
