3c56bfde6c721e8b674990f67435475d4bca5ddf
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-06-26 17:34:51 +0200

    Add skeleton for query execution

---
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/QueryEngine.kt
@@ -0,0 +1,65 @@
+package science.atlarge.grademl.query
+
+import science.atlarge.grademl.query.analysis.ASTAnalysis
+import science.atlarge.grademl.query.language.*
+import science.atlarge.grademl.query.model.DefaultTables
+import science.atlarge.grademl.query.model.Table
+
+class QueryEngine {
+
+    private val tables = DefaultTables.create()
+
+    fun runStatement(statement: Statement) {
+        when (statement) {
+            is SelectStatement -> runSelect(statement)
+        }
+    }
+
+    fun runSelect(selectStatement: SelectStatement) {
+        // Execute the FROM clause
+        val inputTable = applyFrom(selectStatement.from, tables)
+
+        // Execute the WHERE clause
+        val filteredTable =
+            if (selectStatement.where != null) applyWhere(selectStatement.where, inputTable)
+            else inputTable
+
+        // Execute the GROUP BY clause
+        val groupedTable =
+            if (selectStatement.groupBy != null) applyGroupBy(selectStatement.groupBy, filteredTable)
+            else filteredTable
+
+        // Execute the SELECT clause
+        val projectedTable = applySelect(selectStatement.select, groupedTable)
+
+        // Display the result (with the LIMIT clause)
+        TODO()
+    }
+
+    private fun applyFrom(fromClause: FromClause, tables: Map<String, Table>): Table {
+        // Derive a virtual table from (an) existing table(s)
+        TODO()
+    }
+
+    private fun applyWhere(whereClause: WhereClause, table: Table): Table {
+        // Analyze the filter condition
+        val filterExpression = ASTAnalysis.analyzeExpression(whereClause.conditionExpression, table.columns)
+
+        // Filter the input table
+        TODO()
+    }
+
+    private fun applyGroupBy(groupByClause: GroupByClause, table: Table): Table {
+        // Derive a clustered table
+        TODO()
+    }
+
+    private fun applySelect(selectClause: SelectClause, table: Table): Table {
+        // Analyze the projection expressions
+        val projections = selectClause.columnExpressions.map { ASTAnalysis.analyzeExpression(it, table.columns) }
+
+        // Apply the projections
+        TODO()
+    }
+
+}
