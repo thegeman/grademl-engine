7d54b22350461308a7099f9665b62fca1cc66dce
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-10-08 10:16:27 +0200

    Add group-by wrapper to Table

--- grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/Table.kt
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/Table.kt
@@ -2,6 +2,7 @@ package science.atlarge.grademl.query.model
 
 import science.atlarge.grademl.query.language.ColumnLiteral
 import science.atlarge.grademl.query.language.Expression
+import science.atlarge.grademl.query.nextOrNull
 
 interface Table {
 
@@ -9,6 +10,34 @@ interface Table {
 
     // Linear table scan
     fun scan(): RowScanner
+
+    // Grouped table scan
+    val groupedByColumns: List<Column>
+        get() = emptyList()
+    fun scanGroups(): RowGroupScanner {
+        return object : RowGroupScanner() {
+            var used = false
+            override fun fetchRowGroup(): RowGroup? {
+                if (used) return null
+                used = true
+                return object : RowGroup() {
+                    override val columns: List<Column>
+                        get() = this@Table.columns
+                    override val groupedColumnIndices: List<Int>
+                        get() = emptyList()
+                    val baseScanner = this@Table.scan()
+
+                    override fun readGroupColumnValue(columnId: Int, outValue: TypedValue): TypedValue {
+                        throw UnsupportedOperationException()
+                    }
+
+                    override fun fetchRow(): Row? {
+                        return baseScanner.nextOrNull()
+                    }
+                }
+            }
+        }
+    }
 
     // Optimizing transformations
     fun withSubsetColumns(subsetColumns: List<ColumnLiteral>): Table? {
