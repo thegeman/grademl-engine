80e7d9829bd7fd5d069e79454684e1265e166966
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-10-18 00:37:08 +0200

    Add ASTAnalysis.analyzeExpression overload for v2 data model

--- grademl-query/src/main/kotlin/science/atlarge/grademl/query/analysis/ASTAnalysis.kt
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/analysis/ASTAnalysis.kt
@@ -13,4 +13,16 @@ object ASTAnalysis {
         return PatternMatchCompilationPass.rewrite(clonedExpression)
     }
 
+    fun analyzeExpressionV2(expression: Expression, columns: List<science.atlarge.grademl.query.model.v2.Column>): Expression {
+        val clonedExpression = expression.clone()
+        ColumnResolution.resolveColumns(clonedExpression, columns.mapIndexed { index, column ->
+            Column(column.identifier.split('.').last(), column.identifier, index, column.type, column.isKey)
+        })
+        FunctionResolution.resolveFunctionCalls(clonedExpression)
+        TypeChecking.analyzeTypes(clonedExpression, columns.mapIndexed { index, column ->
+            Column(column.identifier.split('.').last(), column.identifier, index, column.type, column.isKey)
+        })
+        return PatternMatchCompilationPass.rewrite(clonedExpression)
+    }
+
 }
