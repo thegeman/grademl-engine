3924017a10391a4e9923f219eb76b147b39f5017
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-10-19 01:14:26 +0200

    Fix schema in physical ProjectPlan

--- grademl-query/src/main/kotlin/science/atlarge/grademl/query/plan/physical/ProjectPlan.kt
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/plan/physical/ProjectPlan.kt
@@ -1,22 +1,31 @@
 package science.atlarge.grademl.query.plan.physical
 
 import science.atlarge.grademl.query.analysis.ASTAnalysis
+import science.atlarge.grademl.query.analysis.ASTUtils
 import science.atlarge.grademl.query.execution.operators.ProjectOperator
 import science.atlarge.grademl.query.execution.operators.QueryOperator
 import science.atlarge.grademl.query.execution.toPhysicalExpression
-import science.atlarge.grademl.query.language.Expression
+import science.atlarge.grademl.query.language.NamedExpression
+import science.atlarge.grademl.query.model.v2.Column
 import science.atlarge.grademl.query.model.v2.TableSchema
 
 class ProjectPlan(
     override val nodeId: Int,
     val input: PhysicalQueryPlan,
-    columnExpressions: List<Expression>
+    columnExpressions: List<NamedExpression>
 ) : PhysicalQueryPlan {
 
-    val columnExpressions = columnExpressions.map { ASTAnalysis.analyzeExpressionV2(it, input.schema.columns) }
+    val columnExpressions = columnExpressions.map { ASTAnalysis.analyzeExpressionV2(it.expr, input.schema.columns) }
 
-    override val schema: TableSchema
-        get() = input.schema
+    override val schema: TableSchema = TableSchema(columnExpressions.mapIndexed { i, namedExpression ->
+        // Find all columns used in the expression
+        val columnsUsed = ASTUtils.findColumnLiterals(namedExpression.expr)
+        // Determine if all input columns are keys
+        val allKeys = columnsUsed.all { input.schema.columns[it.columnIndex].isKey }
+        // Create the new column, mark it as a key column iff all inputs are keys
+        Column(namedExpression.name, this.columnExpressions[i].type, allKeys)
+    })
+
     override val children: List<PhysicalQueryPlan>
         get() = listOf(input)
 
