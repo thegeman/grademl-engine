29a637d0fdc6ad01076e1ae0a65993113ef7e495
Tim Hegeman <tim.m.hegeman@gmail.com>
2021-11-01 16:14:13 +0100

    Add human-readable print functions to Row and TimeSeries for debugging

--- grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/Row.kt
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/Row.kt
@@ -1,5 +1,7 @@
 package science.atlarge.grademl.query.model
 
+import science.atlarge.grademl.query.language.Type
+
 interface Row {
 
     val schema: TableSchema
@@ -7,5 +9,23 @@ interface Row {
     fun getBoolean(columnIndex: Int): Boolean
     fun getNumeric(columnIndex: Int): Double
     fun getString(columnIndex: Int): String
+
+    // Cannot override toString in interface
+    fun debugPrint(): String {
+        val sb = StringBuilder("Row[")
+        schema.columns.forEachIndexed { index, column ->
+            if (index != 0) sb.append(", ")
+            sb.append(column.identifier)
+                .append('=')
+            when (column.type) {
+                Type.BOOLEAN -> sb.append(if (getBoolean(index)) "TRUE" else "FALSE")
+                Type.NUMERIC -> sb.append(getNumeric(index))
+                Type.STRING -> sb.append('"').append(getString(index)).append('"')
+                else -> sb.append("<UNKNOWN>")
+            }
+        }
+        sb.append(']')
+        return sb.toString()
+    }
 
 }
--- grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/TimeSeries.kt
+++ grademl-query/src/main/kotlin/science/atlarge/grademl/query/model/TimeSeries.kt
@@ -1,7 +1,27 @@
 package science.atlarge.grademl.query.model
 
+import science.atlarge.grademl.query.language.Type
+
 interface TimeSeries : Row {
 
     fun rowIterator(): RowIterator
+
+    // Cannot override toString in interface
+    override fun debugPrint(): String {
+        val sb = StringBuilder("TimeSeries[")
+        val keyColumnIndices = schema.columns.withIndex().filter { it.value.isKey }.map { it.index }
+        keyColumnIndices.forEachIndexed { i, columnId ->
+            if (i != 0) sb.append(", ")
+            sb.append(schema.columns[columnId].identifier)
+                .append('=')
+            when (schema.columns[columnId].type) {
+                Type.BOOLEAN -> sb.append(if (getBoolean(columnId)) "TRUE" else "FALSE")
+                Type.NUMERIC -> sb.append(getNumeric(columnId))
+                Type.STRING -> sb.append('"').append(getString(columnId)).append('"')
+                else -> sb.append("<UNKNOWN>")
+            }
+        }
+        return sb.toString()
+    }
 
 }
